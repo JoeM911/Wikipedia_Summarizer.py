0601b4690c8cdd87c57c5bffc3948fe7d806db2b
JoeM911 <154363585+JoeM911@users.noreply.github.com>
2024-02-04 10:51:26 -0600

    streamlit test

--- Wikipedia_Summarizer.py
+++ Wikipedia_Summarizer.py
@@ -1,22 +1,29 @@
+import streamlit as st
 from bs4 import BeautifulSoup
 from transformers import pipeline
 import requests
 
-URL = input("Enter URL:")
-page = requests.get(URL)
+def main():
+     st.title("Wikipedia Summarizer")
 
 
-soup = BeautifulSoup(page.content, 'html.parser')
+URL = st.text_input("Enter URL:")
+if st.button("Summarize"):
+     process_url(URL)
+
+def process_url(URL):
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
 
 #Title Detection
 Title = soup.find('title').get_text()
-print(Title)
+st.header(f"Title: {Title}")
 
 #Body Paragraph Detection
 Body_Parag = soup.find_all('p')
-print("Body Paragraphs:")
+st.subheader("Body Paragraphs:")
 for paragraph in Body_Parag:
-    print(paragraph.text)
+    st.write(paragraph.text)
 
 article_text = ''.join([paragraph.text for paragraph in Body_Parag])
 #Splitting into 512-character segments
@@ -25,12 +32,15 @@ segments = [article_text[i:i + max_length] for i in range(0, len(article_text), 
 #Summarizer Tool
 summarizer = pipeline("summarization", model="Falconsai/text_summarization")
 
-print('Summary of Link:')
+st.subheader('Summary of Link:')
 word_count = 0
 for segment in segments:
         summary = summarizer(segment, max_length=100, min_length=30, do_sample=False)
         summary_text = summary[0]['summary_text']
         word_count += len(summary_text.split())
-        print(summary_text)
+        st.write(summary_text)
         if word_count >= 50:
             break
+
+if __name__ == "__main__": 
+    main()
