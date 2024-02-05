c66ec6776641df902a39c6134fdb695c5f1b1de3
JoeManoj <j566m192@ku.edu>
2024-02-05 13:19:18 -0600

    Updated Project

--- Wikipedia_Summarizer.py
+++ Wikipedia_Summarizer.py
@@ -4,43 +4,49 @@ from transformers import pipeline
 import requests
 
 def main():
-     st.title("Wikipedia Summarizer")
+    st.title("Wikipedia Summarizer")
+    URL = st.text_input("Enter URL:")
+    Fixed_word_count = st.text_input("Enter Word Count:")
 
+    if st.button("Summarize") and Fixed_word_count.strip():
+        process_url(URL, Fixed_word_count)
 
-URL = st.text_input("Enter URL:")
-if st.button("Summarize"):
-     process_url(URL)
-
-def process_url(URL):
+def process_url(URL, fixed_word_count):
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, 'html.parser')
 
-#Title Detection
-Title = soup.find('title').get_text()
-st.header(f"Title: {Title}")
+    # Title Detection
+    Title = soup.find('title').get_text()
+    st.header(f"Title: {Title}")
+    Body_Parag = soup.find_all('p')
+    paragraph_text = ''
+    for paragraph in Body_Parag:
+        paragraph_text += paragraph.get_text() + ' '
 
-#Body Paragraph Detection
-Body_Parag = soup.find_all('p')
-st.subheader("Body Paragraphs:")
-for paragraph in Body_Parag:
-    st.write(paragraph.text)
+    article_text = paragraph_text  # Use the combined paragraph text
+    max_length = 512
+    segments = [article_text[i:i + max_length] for i in range(0, len(article_text), max_length)]
+    summarizer = pipeline("summarization", model="Falconsai/text_summarization")
 
-article_text = ''.join([paragraph.text for paragraph in Body_Parag])
-#Splitting into 512-character segments
-max_length = 512
-segments = [article_text[i:i + max_length] for i in range(0, len(article_text), max_length)]
-#Summarizer Tool
-summarizer = pipeline("summarization", model="Falconsai/text_summarization")
+    # Validate input
+    if not fixed_word_count.isdigit():
+        st.error("Please enter a valid integer for Word Count.")
+        return
 
-st.subheader('Summary of Link:')
-word_count = 0
-for segment in segments:
+    fixed_word_count = int(fixed_word_count)
+    st.write(f"Fixed Word Count: {fixed_word_count}")
+
+    current_word_count = 0
+    summary_text = ""
+    for segment in segments:
         summary = summarizer(segment, max_length=100, min_length=30, do_sample=False)
-        summary_text = summary[0]['summary_text']
-        word_count += len(summary_text.split())
-        st.write(summary_text)
-        if word_count >= 50:
+        current_summary_text = summary[0]['summary_text']
+        current_word_count += len(current_summary_text.split())
+        if current_word_count <= fixed_word_count:
+            summary_text += current_summary_text + " "
+        else:
             break
+    st.write(summary_text)
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     main()
